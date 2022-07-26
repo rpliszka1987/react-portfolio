efbe7385a2309ad4854fc75e4aa0cdd3a5bb2b43
Robert Pliszka <robplisz@gmail.com>
2022-07-26 18:47:24 -0400

    Added nav and footer to the page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import About from "./components/About";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <main>
-        <About></About>
-      </main>
-    </div>
+    <>
+      <div>
+        <Header></Header>
+      </div>
+    </>
   );
 }
 
