27cc13b4adf94966c8d2eae28096a4825aad8988
Robert Pliszka <robplisz@gmail.com>
2022-07-26 18:30:17 -0400

    Added footer to the page

---
+++ src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="page-footer" id="footer">
+      <a target="_blank" href="https://github.com/rpliszka1987">
+        <img
+          src="https://img.icons8.com/ios/80/000000/github.png"
+          alt="github-logo"
+          class="icon"
+        />
+      </a>
+      <a target="_blank" href="https://www.linkedin.com/in/robertpliszka/">
+        <img
+          src="https://img.icons8.com/fluent/80/000000/linkedin-circled.png"
+          alt="linkedin-logo"
+          class="icon"
+        />
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
