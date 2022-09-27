4e869702d2b7396dcea073159d243c8e496dcccb
Robert Pliszka <robplisz@gmail.com>
2022-09-27 13:31:45 -0400

    added new contact area

---
+++ src/components/ContactMe/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ContactMe() => {
+    return(
+        <section className="contact-me">
+            <div>
+                <h2> Contact Me</h2>
+
+            </div>
+            <div>
+                <p>You can reach me via email.</p>
+                <address>
+                    <a href="mailto:robplisz"
+                </address>
+            </div>
+
+        </section>
+    )
+}
